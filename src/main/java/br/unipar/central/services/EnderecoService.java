46db57555a2afcb551010a681a4665c9a8736dd5
LucasFigueredoo <131011037+LucasFigueredoo@users.noreply.github.com>
2023-06-10 11:35:36 -0300

    Atividade 3 de recursividade

--- src/main/java/br/unipar/central/services/EnderecoService.java
+++ src/main/java/br/unipar/central/services/EnderecoService.java
@@ -4,10 +4,69 @@
  */
 package br.unipar.central.services;
 
+import br.unipar.central.exceptions.CampoNaoInformadoException;
+import br.unipar.central.exceptions.EntidadeNaoInformadaException;
+import br.unipar.central.exceptions.TamanhoCampoInvalidoException;
+import br.unipar.central.models.Endereco;
+
 /**
  *
  * @author 01330
  */
 public class EnderecoService {
+    
+    private void validar(Endereco endereco) throws EntidadeNaoInformadaException, CampoNaoInformadoException, TamanhoCampoInvalidoException {
+        
+        if (endereco == null) {
+            throw new EntidadeNaoInformadaException("Endereco");
+        }
+        
+        if (endereco.getLogradouro() == null ||
+            endereco.getLogradouro().isEmpty() ||
+            endereco.getLogradouro().isBlank()) {
+            throw new CampoNaoInformadoException("Logradouro");
+        }
+        
+        if (endereco.getNumero() == null ||
+            endereco.getNumero().isEmpty() ||
+            endereco.getNumero().isBlank()) {
+            throw new CampoNaoInformadoException("Numero");
+        }
+        
+        if (endereco.getBairro() == null ||
+            endereco.getBairro().isEmpty() ||
+            endereco.getBairro().isBlank()) {
+            throw new CampoNaoInformadoException("Bairro");
+        }
+        
+        if (endereco.getCep() == null ||
+            endereco.getCep().isEmpty() ||
+            endereco.getCep().isBlank()) {
+            throw new CampoNaoInformadoException("CEP");
+        }
+        
+        if (endereco.getComplemento() == null ||
+            endereco.getComplemento().isEmpty() ||
+            endereco.getComplemento().isBlank()) {
+            throw new CampoNaoInformadoException("Complemento");   
+        }
+        
+        if (endereco.getPessoa() == null) {
+            throw new CampoNaoInformadoException("Pessoa");
+        }
+        
+        if (endereco.getCidade() == null) {
+            throw new CampoNaoInformadoException("Cidade");
+        }
+        
+        if (endereco.getLogradouro().length() > 120) {
+            throw new TamanhoCampoInvalidoException("Logradouro", 120);
+        }
+        
+        if (endereco.getNumero().length() > 10) {
+            throw new TamanhoCampoInvalidoException("Numero", 10);
+        }
+        
+    }
     
 }

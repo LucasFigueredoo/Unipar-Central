831a298f7fcf83c74edf1757f12d94ca06a1f0e8
LucasFigueredoo <131011037+LucasFigueredoo@users.noreply.github.com>
2023-06-11 20:25:40 -0300

    Atividade 3 de recursividade

--- src/main/java/br/unipar/central/repositories/AgenciaDAO.java
+++ src/main/java/br/unipar/central/repositories/AgenciaDAO.java
@@ -6,8 +6,16 @@ package br.unipar.central.repositories;
 
 /**
  *
- * @author Lucas
+ * @author 00240578
  */
 public class AgenciaDAO {
+    
+    public static final String INSERT =
+            "INSERT INTO AGENCIA (ID, CODIGO, DIGITO, RAZAOSOCIAL, CNPJ, RA, BANCO_ID) " +
+            "VALUES(?, ?, ?, ?, ?, ?, ?)";
+    
+    public static final String FIND_ALL =
+            "SELECT ID, CODIGO, DIGITO, RAZAOSOCIAL, CNPJ, RA, BANCO_ID " +
+            "FROM AGENCIA";
     
 }
